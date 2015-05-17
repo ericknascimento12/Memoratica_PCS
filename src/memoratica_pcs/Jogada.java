1dc1cb8e7d00f3a57b66bc903623f9eebea58743
ericknascimento12 <ericknascimento12@gmail.com>
2015-05-17 16:31:16 -0300

    Memoratica

---
+++ src/memoratica_pcs/Jogada.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoratica_pcs;
+
+/**
+ *
+ * @author Erick
+ */
+public class Jogada {
+    
+    //Atributo
+    Peca peca1;
+    Peca peca2;
+    
+    //Métodos
+    
+    int ContaJogada(){
+        return 0;
+}
+    Jogada () {}
+    
+    
+    
+    
+}
