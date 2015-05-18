97fd12815e2d2e6279138f37f2bc25bf011a1824
ericknascimento12 <ericknascimento12@gmail.com>
2015-05-18 00:51:29 -0300

    Memoratica

--- src/memoratica_pcs/Usuario.java
+++ src/memoratica_pcs/Usuario.java
@@ -9,18 +9,57 @@ package memoratica_pcs;
  *
  * @author Gian
  */
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
 public class Usuario {
      // Atributos
     private int id_usuario;
     private String nome;
     private char modulo_liberado;
+    private static final String USUARIO_XML = "usuario.xml";
+
     
     //Construtor
     public Usuario (String nome,int id_usuario)
     {
         this.nome=nome;
         this.id_usuario=id_usuario;
+
     }
+    
+    public void salvarUsuario(Usuario usuario) throws IOException {
+        try (XMLEncoder xmlEncoder = new XMLEncoder(new FileOutputStream(USUARIO_XML))) {
+            xmlEncoder.writeObject(usuario);
+        }
+    }
+ public Usuario verificarUsuario() throws IOException {
+        Usuario usuario = new usuario();
+        File arquivo = new File(USUARIO_XML);
+        if (arquivo.exists()) {
+            try (XMLDecoder xmlDecoder = new XMLDecoder(new FileInputStream(ORTOGRAFIA_XML))) {
+                jogo = (JogoOrtografia) xmlDecoder.readObject();
+            }
+        }    
+
+    
     //Metodos
     
     
